b0c00e1167a2b5f5fe4360bac82483b244101164
David Efod <davidefod@gmail.com>
2023-04-06 13:13:41 +0000

    3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-'''
-fabric script to distribute an archive to web servers
-----NEEDS TO REVISIT SCRIPT
+''' fcreates and distributes an archive to your web servers, using deploy():
 '''
 
 import os
 from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
-env.hosts = ['54.89.28.248', '52.87.41.147']
+
+env.hosts = ['', '']
 
 
-def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-      archive_path (str): The path of the archive to distribute.
-    Returns:
-      If the file doesn't exist at archive_path or an error occurs - False.
-      Otherwise - True.
-    """
+@runs_once
+def do_pack():
+    """Archives the static files."""
     if not os.path.isdir("versions"):
         os.mkdir("versions")
     cur_time = datetime.now()
@@ -33,7 +27,12 @@ def do_deploy(archive_path):
     try:
         print("Packing web_static to {}".format(output))
         local("tar -cvzf {} web_static".format(output))
-        archive
+        archive_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
+
 
 def do_deploy(archive_path):
     """Deploys the static files to the host servers.
@@ -55,8 +54,15 @@ def do_deploy(archive_path):
         run("rm -rf {}web_static".format(folder_path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(folder_path))
-        print("New version deployed!")
+        print('New version is now LIVE!')
         success = True
     except Exception:
         success = False
     return success
+
+
+def deploy():
+    """Archives and deploys the static files to the host servers.
+    """
+    archive_path = do_pack()
+    return do_deploy(archive_path) if archive_path else False
